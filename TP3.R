fbb84c83cf53173019a2961cc6073d462409a8f1
jbdouet <jean.baptiste.douet@gmail.com>
2017-11-13 23:12:00 +0100

    recommit

--- TP3.R
+++ TP3.R
@@ -48,10 +48,11 @@ plot(yhat_scale,data_reg_scaled.test$y)
 abline(0,1)
 
 ####   Pcr   ####
+fitControl <- trainControl(method = "cv",number = 10)
+grid <- expand.grid(ncomp=c(3,5,10,20, 30,36,50))
+model_pcr <- caret::train(data_reg.train[,-51],data_reg.train$y,method='pcr',trControl= fitControl,tuneGrid=grid)
 
-model_pcr <- caret::train(data_reg.train[,-51],data_reg.train$y,method='pcr',
-                          trainControl(method = "cv", number = 3, returnResamp = "all"))
-
+model_pcr$bestTune
 #pour voir quels paramètres peuvent être "tuned":
 modelLookup(model='pcr') # no parameters to tune
 
@@ -64,7 +65,28 @@ predictions_pcr<-predict.train(object=model_pcr,data_reg.test[,-51],type="raw")
 mean((data_reg.test$y-predictions_pcr)^2)
 plot(predictions_pcr,data_reg.test$y)
 abline(0,1)
-summary(model_pcr)
+
+### Lasso  ###
+
+fitControl <- trainControl(method = "cv",number = 10)
+model_lasso <- caret::train(data_reg.train[,-51],data_reg.train$y,method='lasso',trControl= fitControl)
+
+model_lasso$bestTune
+#pour voir quels paramètres peuvent être "tuned":
+modelLookup(model='lasso') # no parameters to tune
+
+plot(model_lasso)# does not work if no parameters to tune
+
+plot(varImp(object=model_lasso),main="PCR - Variable Importance")
+
+predictions_lasso<-predict.train(object=model_lasso,data_reg.test[,-51],type="raw")
+
+data_reg.test$y==data_reg.test[,-51]
+
+mean((data_reg.test$y-predictions_lasso)^2)
+plot(predictions_lasso,data_reg.test$y)
+abline(0,1)  
+
 ################## CLASSIFICATION ############
 
 data_clas<-read.csv('data/tp3_clas_app.txt',sep=' ')
